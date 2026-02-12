a4347b271e57ff68c9e5a7f638bf365571a08db8
mozikr-dev <szymon.mozol.dev@gmail.com>
2026-02-12 19:47:27 +0100

    Dynamic list

--- game-library-frontend/src/Components/ListGroup.tsx
+++ game-library-frontend/src/Components/ListGroup.tsx
@@ -8,16 +8,14 @@ function ListGroup(){
         'Berlin'
     ];
 
+
     return(
         <>
             <h1>List of jaja czyste</h1>
             <ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
+                {items.map(item => <li key={item}>{item}</li>)}
             </ul>
+
         </>
         )
 }
